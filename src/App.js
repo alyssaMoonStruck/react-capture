4a64ba3245898317ea5365ffce75379c26c779e7
Alyssa DeMay <camocat11@gmail.com>
2020-12-22 00:24:01 -0600

    .10 - 12 Movie details

--- src/App.js
+++ src/App.js
@@ -4,14 +4,32 @@ import Nav from './components/Nav'
 import GlobalStyle from './components/GlobalStyle'
 //Import Pages
 import AboutUs from './pages/AboutUs'
+import OurWork from './pages/OurWork'
+import ContactUs from './pages/ContactUs'
+import MovieDetail from './pages/MovieDetail'
+//Router
+import { Switch, Route } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
-      <AboutUs />
-      <Nav />
       <GlobalStyle />
+      <Nav />
+      <Switch>
+      <Route path="/" exact>
+        <AboutUs />
+      </Route>
+      <Route path="/work" exact>
+        <OurWork />
+      </Route>
+      <Route path="/work/:id">
+        <MovieDetail />
+      </Route>
+      <Route path="/contact">
+        <ContactUs />
+      </Route>
+      </Switch>
     </div>
   );
 }
