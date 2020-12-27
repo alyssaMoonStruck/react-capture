828bee30794a594d919e4fc869e295a462acab7e
Alyssa DeMay <camocat11@gmail.com>
2020-12-27 17:06:52 -0600

    Animating Wave

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -38,25 +38,25 @@ const OurWork = () => {
 }
 
 const Work = styled(motion.div)`
-    min-height: 100vh;
-    overflow: hidden;
-    padding: 5rem 10rem;
-    h2{
-        padding: 1rem 0rem;
-    }
-`;
+min-height: 100vh;
+overflow: hidden;
+padding: 5rem 10rem; 
 
-const Movie = styled.div`
-    padding-bottom: 10rem;
-    .line{
-        height: 0.5rem;
-        background: #cccccc;
-        margin-bottom: 3rem;
-    }
-    img {
-        width:100%100vh;
-        height: 70vh;
-        object-fit: cover;
-    }
+h2 {
+padding: 1rem 0rem;
+}
+`;
+const Movie = styled(motion.div)`
+padding-bottom: 10rem;
+.line {
+height: 0.5rem;
+background: #23d997;
+margin-bottom: 3rem;
+}
+img {
+width: 100%;
+height: 70vh;
+object-fit: cover;
+}
 `;
 export default OurWork
