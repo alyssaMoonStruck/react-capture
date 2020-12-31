8cee0a248a35b7448f981061bce3ca7a3c5ab7fe
Alyssa DeMay <camocat11@gmail.com>
2020-12-30 22:18:36 -0600

    .18 Adding Toggle

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,79 +1,77 @@
 import React from 'react'
 import styled from 'styled-components'
 import { About } from '../styles'
+import Toggle from './Toggle'
+import { AnimateSharedLayout } from "framer-motion"
 
 
 const FaqSection = () => {
     return (
-
         <Faq>
-            <h2>Any Questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I Start?</h4>
-                <div className="answer">
-                    <p>On the road to viridin city 2.B a Master jam sesh</p>
-                    <p>Lorem ipsumwr ghrubw jahvueg yrisgi sigugisgisgse 
-                        isgy sug sugsf.
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    <p>On the road to viridin city 2.B a Master jam sesh</p>
-                    <p>Lorem ipsumwr ghrubw jahvueg yrisgi sigugisgisgse 
-                        isgy sug sugsf.
-                    </p>
-                    <div className="faq-line"></div>
-                </div>
-            </div><div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    <p>On the road to viridin city 2.B a Master jam sesh</p>
-                    <p>Lorem ipsumwr ghrubw jahvueg yrisgi sigugisgisgse 
-                        isgy sug sugsf.
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div><div className="question">
-                <h4>What Products Do You Offer?</h4>
-                <div className="answer">
-                    <p>On the road to viridin city 2.B a Master jam sesh</p>
-                    <p>Lorem ipsumwr ghrubw jahvueg yrisgi sigugisgisgse 
-                        isgy sug sugsf.
-                    </p>
-                </div>
-            </div>
+            <h2> Any Questions <span>FAQ</span> </h2>
+            <AnimateSharedLayout>
+                <Toggle title='How Do I Start'>
+                    <div className="answer">
+                        <p>On the road to viridin city 2.B a Master jam sesh</p>
+                        <p>Lorem ipsumwr ghrubw jahvueg yrisgi sigugisgisgse 
+                            isgy sug sugsf.
+                        </p>
+                    </div>
+                </Toggle>
+                <Toggle title='Daily Schedule'>
+                    <div className="answer">
+                        <p>On the road to viridin city 2.B a Master jam sesh</p>
+                        <p>Lorem ipsumwr ghrubw jahvueg yrisgi sigugisgisgse 
+                            isgy sug sugsf.
+                        </p>
+                    </div>
+                </Toggle>
+                <Toggle title='Different Payment Methods'>
+                    <div className="answer">
+                        <p>On the road to viridin city 2.B a Master jam sesh</p>
+                        <p>Lorem ipsumwr ghrubw jahvueg yrisgi sigugisgisgse 
+                            isgy sug sugsf.
+                        </p>
+                    </div>
+                </Toggle>
+                <Toggle title='What Products Do You Offer?'>
+                    <div className="answer">
+                        <p>On the road to viridin city 2.B a Master jam sesh</p>
+                        <p>Lorem ipsumwr ghrubw jahvueg yrisgi sigugisgisgse 
+                            isgy sug sugsf.
+                        </p>
+                    </div>
+                </Toggle>
+            </AnimateSharedLayout>
         </Faq>
     )
 }
 
 const Faq = styled(About)`
-display: block;
-span{
-    display: block
-}
-h2{
-    padding-bottom: 2rem;
-    font-weight: lighter;
-}
-.faq-line {
-    background: #cccccc;
-    height: 0.2rem;
-    margin: 2rem 0rem;
-    width: 100%;
-}
-.question {
-    padding: 3rem 0rem;
-    cursor: pointer;
-}
-.answer{
-    padding: 2rem 0rem 
-    p {
-        padding: 1rem 0rem;
+    display: block;
+    span{
+        display: block;
     }
-}
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer{
+        padding: 2rem 0rem ;
+        p {
+            padding: 1rem 0rem;
+        }
+    }
 `;
 
 export default FaqSection
