b479aeb6b6b0e8151a9f55c057f77a0c0f7324d9
Alyssa DeMay <camocat11@gmail.com>
2021-01-05 22:58:02 -0600

    .23 Making app mobile friendly;

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -53,6 +53,19 @@ li {
 padding-left: 10rem;
 position: relative;
 }
+@media (max-width: 1300px) {
+flex-direction: column;
+padding: 2rem 1rem;
+#logo {
+display: inline-block;
+margin: 2rem;
+}
+ul {
+padding: 2rem;
+justify-content: space-around;
+width: 100%;
+    }
+}
 `;
 
 export default Nav;
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -72,7 +72,10 @@ const Awards = styled.div`
     margin: 5rem 20rem;
     align-items: center;
     justify-content: space-between;
-
+    @media (max-width: 1500px) {
+        margin-top: 2rem 2rem;
+        display: block;
+    }
 `;
 
 const AwardStyle = styled.div`
